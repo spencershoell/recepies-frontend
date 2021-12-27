b3a7305d96fef1ad6dfdba079ae3e2c4bfd872b6
Spencer Shoell <spencer@shoell.com>
2021-12-27 15:03:50 -0800

    added recipe deatil page

--- src/app/pages/recipe/recipe-detail.component.ts
+++ src/app/pages/recipe/recipe-detail.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { Component, NgModule, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { DxDataGridModule } from "devextreme-angular";
+import { DxButtonModule, DxDataGridModule, DxFormModule, DxTabPanelModule } from "devextreme-angular";
 import CustomStore from "devextreme/data/custom_store";
 import DataSource from "devextreme/data/data_source";
 import { Subscription } from "rxjs";
 import { Recipe } from "src/app/models";
 import { CourseStoreService } from "src/app/stores/course.store";
 import { RecipeStoreService } from "src/app/stores/recipe.store";
+import { faEdit } from '@fortawesome/pro-solid-svg-icons';
 
 @Component({
     selector: 'app-recipe-detail',
@@ -15,15 +16,38 @@ import { RecipeStoreService } from "src/app/stores/recipe.store";
     styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit, OnDestroy {
+    faEdit = faEdit;
+
     private _routerSubscription!: Subscription;
     private _recipe!: Recipe;
     private _recipeId!: string;
 
+    private _detailEditing: boolean = false;
+
     dataSource: DataSource;
     courses: CustomStore;
 
+    tabItems: Array<any> = [
+        {
+            title: 'Details',
+            template: 'detailsTemplate'
+        },
+        {
+            title: 'Ingredients',
+            template: 'ingredientsTemplate'
+        },
+        {
+            title: 'Instructions',
+            template: 'instructionsTemplate'
+        }
+    ];
+
     get recipe(): Recipe {
         return this._recipe;
+    }
+
+    get detailEditing(): boolean {
+        return this._detailEditing;
     }
 
     constructor(
@@ -48,14 +72,27 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
         this._routerSubscription.unsubscribe();
     }
 
+    async detailCancel() {
+        await this.loadRecipe();
+        this._detailEditing = false;
+    }
+
+    detailEdit() {
+        this._detailEditing = true;
+    }
+
+    async detailSave() {
+        await this.recipeStoreSerivce.update(this.recipe.id, this.recipe);
+        this._detailEditing = false;
+    }
+
     private async loadRecipe(): Promise<void> {
-        console.log(this._recipeId);
-        this._recipe = await this.recipeStoreSerivce.byKey(this._recipeId);  
+        this._recipe = await this.recipeStoreSerivce.byKey(this._recipeId);
     }
 }
 
 @NgModule({
-    imports: [CommonModule, DxDataGridModule],
+    imports: [CommonModule, DxButtonModule, DxDataGridModule, DxFormModule, DxTabPanelModule],
     declarations: [RecipeDetailComponent],
     exports: [RecipeDetailComponent]
 })
